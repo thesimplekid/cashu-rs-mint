845cca5b6e83e068cf46e7d5376cf3b748487e7e
thesimplekid <tsk@thesimplekid.com>
2024-01-16 23:42:16 +0000

    chore: update flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A flake for developing cashu-rs-mint";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
 
     flakebox = {
       url = "github:rustshop/flakebox";
